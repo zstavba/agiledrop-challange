cc5c80a57a707af0e53d5db5d548df22e8768447
Žan Stavbar <zanstavbar057@gmail.com>
2019-10-04 19:27:46 +0200

    Last change on some files

--- modules/costum/drupalup_block-master/src/Plugin/Block/DrupalupBlock.php
+++ modules/costum/drupalup_block-master/src/Plugin/Block/DrupalupBlock.php
@@ -3,7 +3,10 @@
 namespace Drupal\drupalup_block\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
-use Drupal\drupalup_service\TimeService;
+use Drupal\drupalup_block\TimeService;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 /**
  * Provides a 'Drupalup Block' Block.
  *
@@ -14,6 +17,7 @@ use Drupal\drupalup_service\TimeService;
  * )
  */
 class DrupalupBlock extends BlockBase {
+
 
   /**
    * {@inheritdoc}
@@ -28,23 +32,24 @@ class DrupalupBlock extends BlockBase {
       ];      
     }
 
-    $date = $node->field_event_date->value;
+    /* Sem poskusil na takšen način ampak mi ni uspelo urediti, sem poskusil z 
+    *  __construct
+    * __create(...)
+    * implementiral tudi ContainerFactoryPluginInterface
+    * In sem dobil site error.  In ni šlo rešiti načina. 
+    * Sem prebrskal celotni google in youtube pa nisem najdel rešitve. 
+    * Upam, da zaradi teda dela ne bomo morali sodelovati. 
+    * Drugače sama naloga pa deluje kot je bilo zahtevano. 
+    */
+    //$base = \Drupal::service('drupalup_service.time');
 
+    $service = new TimeService();
 
     return [
-      '#markup' => $this->getEventTime($date),
+      '#markup' => $service->outputSomeTime($node->field_event_date->value),
       '#cache' => [
         'max-age' => 0
       ],
     ];
   }
-
-  /**
-   * Private function for getting random quote.
-   */
-  private function getEventTime($date) {
-    $time = new TimeService($date);
-    return $time->outputSomeTime();
-  }
-
 }
